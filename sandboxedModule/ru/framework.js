2f8945888795007eb27c675fc3ea66f8791acb12
TanyaOhotnik <tanyaohotnik3003@gmail.com>
2016-03-17 15:56:19 +0200

    1.4

--- sandboxedModule/ru/framework.js
+++ sandboxedModule/ru/framework.js
@@ -9,33 +9,36 @@ var fs = require('fs'),
 
 // Create a hash and turn it into the sandboxed context which will be
 // the global context of an application
-var context = { module: {}, console: console, setTimeout: setTimeout, setInterval:setInterval, clearInterval:clearInterval, util:util };
-context.global = context;
-var sandbox = vm.createContext(context);
-// Read an application source code from the file
+var context = { module: {}, console: console, setTimeout: setTimeout, setInterval:setInterval, clearInterval:clearInterval, util:util,
+console:{
 
-
-var array;
-var begin = "./", end = ".js";
-process.argv.forEach(function(val, index) {
-
-if (index>1){
-var fileName = begin + val + end;
-// console.log(index + " : " + begin);
-// var fileName = './application.js';
-var sandbox = vm.createContext(context);
-fs.readFile(fileName, function(err, src) {
-// Тут нужно обработать ошибки
-
-// Запускаем код приложения в песочнице
-var script = vm.createScript(src, fileName);
-script.runInNewContext(sandbox);
-
-// Забираем ссылку из sandbox.module.exports, можем ее исполнить,
-// сохранить в кеш, вывести на экран исходный код приложения и т.д.
-});
+log: function(message){
+var date = new Date();
+if(process.argv.length == 3){
+applicationName = path.basename(process.argv[2]);
+}
+else{
+applicationName = "application";
+}
+var time = date.getDate() + ':' + (date.getMonth()+1) + ':' + date.getFullYear() + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+console.log(applicationName + ' ' + time + ': ' + message);
 }
 
 
+}
+};
+context.global = context;
+var sandbox = vm.createContext(context);
+// Read an application source code from the file
+var fileName = './application.js';
+fs.readFile(fileName, function(err, src) {
+  // We need to handle errors here
+  
+  // Run an application in sandboxed context
 
+  var script = vm.createScript(src, fileName);
+  script.runInNewContext(sandbox);
+  
+  // We can access a link to exported interface from sandbox.module.exports
+  // to execute, save to the cache, print to console, etc.
 });
